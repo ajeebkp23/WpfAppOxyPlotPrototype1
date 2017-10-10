df0bcfa7f6103b1c171ae516dd7f7d82eb04856b
Ajeeb K P <ajeeb@spiatech.com>
2017-10-10 17:33:59 +0530

    First plotting sample shows a sine wave

---
+++ WpfAppOxyPlotPrototype1/MainPlotViewModel1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace WpfAppOxyPlotPrototype1
+{
+    class MainPlotViewModel1
+    {
+        public MainPlotViewModel1()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
